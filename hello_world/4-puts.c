f53c68f944bd0e0e9cdccab10060ccc30b2e47bc
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-20 15:38:35 +0000

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
-/** 
- * main - Entry point
- * Return: Alvays 0 (Success)
- */
+/*
+ *main -Entry point
+ *Return: Alvay 0 (Success)
+*/
 int main(void)
-
 {
-      puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
