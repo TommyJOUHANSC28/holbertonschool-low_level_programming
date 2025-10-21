5ef668be4bb05ca74d8860e95da2eb808d3f755e
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-21 11:36:50 +0000

    Print numberz C

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*  main - play all single digit numbers of base 10 starting from 0.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
