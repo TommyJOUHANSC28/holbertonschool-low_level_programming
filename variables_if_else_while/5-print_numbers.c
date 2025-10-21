b6a56009b1ffd4429c99d3224fa442037d1ba028
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-21 10:56:45 +0000

    Print numbers C

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*  main - play what I call the number of 0 at 9.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+putchar('\n');
+return (0);
+}
