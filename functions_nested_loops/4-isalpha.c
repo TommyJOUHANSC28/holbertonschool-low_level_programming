d5478740defc1ee4af798608906c0daed8e2304a
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-22 11:01:07 +0000

    Isalpha C

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isalpha - Checks for alphabetic characte
+* @c: arg to be checked
+*
+* Return: 1 for alpha, 0 for non-alpha
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
