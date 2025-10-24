8f126ac05dc845241b83d85e18b37a29dcde64dd
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-24 08:08:57 +0000

    Natural

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,24 +1,26 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
-* print_times_table - Affiche la table de multiplication de n
-* @n: entier entre 0 et 15
-*/
-void print_times_table(int n)
+ * main - Program to sum integers
+ *
+ * Description: check for all natural numbers in range of
+ * 3 and 5 and sum them
+ * Return: 0
+ */
+
+int main(void)
 {
-int i, j;
-if (n < 0 || n > 15)
-return;
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-int prod = i * j;
-if (j == 0)
-printf("%d", prod);
-else
-printf(", %3d", prod);
-}
-printf("\n");
-}
+
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
