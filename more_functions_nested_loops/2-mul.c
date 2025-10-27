68f556584ddf414da6c9486fa81940fa1b7ebed4
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-27 10:04:51 +0000

    Multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - function checks for that multiplies two integers.
+* @a: a - Variable
+* @b: b - Variable
+* Return: Always 0.
+*/
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
