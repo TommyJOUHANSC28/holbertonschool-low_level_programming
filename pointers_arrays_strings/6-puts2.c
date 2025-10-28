a5ffe03b7d963df2301699ee78f46d0395e88369
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-28 19:39:55 +0000

    Puts 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,11 @@
 */
 void puts2(char *str)
 {
-while (*str)
-{
-_putchar(*str);
-str++;
-}
-str = '\0';;
+int len = 0, i = 0;
+while (str[len] != '\0')
+len++;
+len -= 1;
+for (; i <= len; i += 2)
+_putchar(str[i]);
+_putchar('\n');
 }
