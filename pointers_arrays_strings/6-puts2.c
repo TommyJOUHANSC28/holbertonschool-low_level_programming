ecea9f64467f266ac9d1b1e0b2ec799c18d7d2c6
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-28 15:28:05 +0000

    Puts half

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* puts2 - A function that prints every other character of a string
+* @str: The string to print every other character of a string
+*
+* Return: Nothing
+*/
+void puts2(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
