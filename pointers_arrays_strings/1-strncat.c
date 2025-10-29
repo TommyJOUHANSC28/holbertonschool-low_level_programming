91e0f59c887f1beb91407b89997602928c0da0de
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-29 09:30:16 +0000

    Strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strncat - A fontion that concatenates two strings
+* @dest: an input string
+* @src: an input string
+* @n: an input integer
+*
+* Return: A pointer to the resulting string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int srclen = 0, i = 0;
+char *temp = dest, *star = src;
+while (*src)
+{
+srclen++;
+src++;
+}
+while (*dest)
+dest++;
+if (n > srclen)
+n = srclen;
+src =  star;
+for (; i < n; i++)
+*dest++ = '\0';
+return (temp);
+}
