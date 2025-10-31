effcf94c6b38c8a6cd62b72455e26265cbf434f0
Tommy JOUHANS <12119@holbertonstudents.com>
2025-10-31 14:45:52 +0000

    Prime Factor

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,45 +1,26 @@
 #include <stdio.h>
 /**
-* maxPrimeFactors - Finds the largest prime factor of a number.
-* @n: interger number
-* Return: The largest prime factor.
-*/
-long maxPrimeFactors(long n)
+ * main - Entry point
+ *
+ * Return: Always largest_prime (Success)
+ */
+int main(void)
 {
+long prime = 612852475143;
 long largest_prime = -1, i = 2;
-while (i * i <= n)
+while (i * i <= prime)
 {
-while (n % i == 0)
+while (prime % i == 0)
 {
 largest_prime = i;
-n = n / i;
+prime = prime / i;
 }
 i = i + 1;
 }
-if (n > 1)
+if (prime > 1)
 {
-largest_prime = n;
+largest_prime = prime;
 }
+printf("%ld\n", largest_prime);
 return (largest_prime);
-}
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-long n = 2;
-printf("%ld\n", maxPrimeFactors(n));
-n = 2;
-printf("%ld\n", maxPrimeFactors(n));
-n = 2;
-printf("%ld\n", maxPrimeFactors(n));
-n = 2;
-printf("%ld\n", maxPrimeFactors(n));
-n = 37;
-printf("%ld\n", maxPrimeFactors(n));
-n = 612852475143;
-printf("%ld\n", maxPrimeFactors(n));
-return (0);
 }
