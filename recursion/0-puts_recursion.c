3ae9648981963a63817e5c235470c897a4394aa2
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-04 12:43:07 +0000

    Put recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _puts_recursion - Prints a string
 *
@@ -10,9 +11,9 @@ void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-putchar('\n');
+_putchar('\n');
 return;
 }
-putchar(*s);
+_putchar(*s);
 _puts_recursion(s + 1);
 }
