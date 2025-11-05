2a10e2c8b0088ef25f936aa08aa1ba72c04a7adb
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-05 16:16:31 +0000

    Add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
 * main -  A program that multiplies two numbers
 * @argc: Number of arguments
@@ -9,10 +10,23 @@
 */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
+int i, j;
+int sum = 0;
+if (argc < 1)
+{
+printf("0\n");
+}
 for (i = 1; i < argc; i++)
 {
 sum += atoi(argv[i]);
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!(isdigit(argv[i][j])))
+{
+printf("Error\n");
+return (0);
+}
+}
 }
 printf("%d\n", sum);
 return (0);
