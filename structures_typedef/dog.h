ee6981080a6fb476565e7446190338b3b760c28e
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-12 09:50:14 +0000

    Dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for dog info
+ *
+ * @name: type char
+ * @age: float type
+ * @owner: char type
+ *
+ *Description: structure to store dog info
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
+
