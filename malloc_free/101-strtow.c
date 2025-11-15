deaf1585f88409c35da0cacaff13dc7ce66a58e1
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-15 21:37:01 +0000

    Strtow

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -13,10 +13,12 @@ char **w = malloc((wc + 1) * sizeof(char *));
 if (!str || !*str)
 return (NULL);
 while (str[i])
-wc += (str[i] != ' ' && !in)
-? (in = 1) : (str[i++] == ' ' && in ? in = 0 : 0);
-if (!wc || !w)
-return (NULL);
+{
+if (str[i] != ' ' && !in)
+wc++, in = 1;
+else if (str[i++] == ' ' && in)
+in = 0;
+}
 i = 0, j = 0, in = 0;
 while (str[i])
 {
@@ -24,25 +26,26 @@ if (str[i] != ' ')
 {
 if (!in)
 {
-for (len = 0; str[i + len] && str[i + len] != ' '; len++)
-if (!w[j])
+len = 0;
+while (str[i + len] && str[i + len] != ' ')
+len++;
 w[j] = malloc(len + 1);
+if (!w[j])
+{
 while (j--)
 free(w[j]);
 free(w);
 return (NULL);
+}
 for (k = 0; k < len; k++)
 w[j][k] = str[i + k];
 w[j++][k] = '\0';
-i += len;
 in = 1;
 }
-else
 i++;
 }
 else
-in = 0;
-i++;
+in = 0, i++;
 }
 w[j] = NULL;
 return (w);
