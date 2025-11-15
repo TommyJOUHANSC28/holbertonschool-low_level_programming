f0c6f30d427b2766a6596bef1846185b25dacade
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-15 20:49:36 +0000

    Strtow

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -7,48 +7,47 @@
 */
 char **strtow(char *str)
 {
-int i = 0, j = 0, k, wc = 0, in_word = 0, len = 0;
-char **words;
+int i = 0, j = 0, k, len, wc = 0, in = 0;
+char **w = malloc((wc + 1) * sizeof(char *));;
 if (!str || !*str)
 return (NULL);
 while (str[i])
 {
-if (str[i] != ' ' && !in_word)
-wc++, in_word = 1;
-else if (str[i] == ' ')
-in_word = 0;
-i++;
+if (str[i] != ' ' && !in)
+wc++, in = 1;
+else if (str[i++] == ' ' && in)
+in = 0;
 }
-if (wc == 0)
-return (NULL);
-words = malloc(sizeof(char *) * (wc + 1));
-if (!words)
-return (NULL);
-i = 0, in_word = 0;
+i = 0, j = 0, in = 0;
 while (str[i])
 {
-if (str[i] != ' ' && !in_word)
+if (str[i] != ' ')
 {
-for (len = 0; str[i + len] && str[i + len] != ' '; len++)
-words[j] = malloc(len + 1);
-if (!words[j])
+if (!in)
+{
+len = 0;
+while (str[i + len] && str[i + len] != ' ')
+len++;
+w[j] = malloc(len + 1);
+if (!w[j])
 {
 while (j--)
-free(words[j]);
-free(words);
+free(w[j]);
+free(w);
 return (NULL);
 }
 for (k = 0; k < len; k++)
-words[j][k] = str[i + k];
-words[j++][k] = '\0';
-i += len;
-in_word = 1;
+w[j][k] = str[i + k];
+w[j++][k] = '\0';
+in = 1;
 }
-else
-in_word = (str[i] != ' ') ? 1 : 0;
 i++;
 }
-words[j] = NULL;
-return (words);
+else
+in = 0, i++;
 }
+w[j] = NULL;
+return (w);
+}
+
 
