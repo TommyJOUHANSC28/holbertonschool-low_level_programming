fc7ee2e56e9c7e9247fbc533c033b11da1dfed75
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-16 19:04:51 +0000

    Multiplication alloc

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -2,6 +2,21 @@
 #include <math.h>
 #include <stdlib.h>
 /**
+* mult_alloc - Multiply two integers with malloc
+* @a: first integer
+* @b: second integer
+* Return: pointer to result (int), or NULL if malloc fails
+*/
+int *mult_alloc(int a, int b)
+{
+int *res;
+res = malloc(sizeof(int));
+if (res == NULL)
+return (NULL);
+*res = a * b;
+return (res);
+}
+/**
 * main - Check the code
 * @argc: number of arguments
 * @argv: arguments
@@ -9,18 +24,19 @@
 */
 int main(int argc, char **argv)
 {
-int numero1, numero2, resultat;
+int *p;
 if (argc != 3)
 {
-printf("Error\n");
+fprintf(stderr, "Error: Usage %s numero 1 numuro 2\n", argv[0]);
 exit(98);
 }
-else
+p = mult_alloc(atoi(argv[1]), atoi(argv[2]));
+if (p == NULL)
 {
-numero1 = atoi(argv[1]);
-numero2 = atoi(argv[2]);
-resultat = numero1 * numero2;
-printf("%d\n", resultat);
-return (0);
+fprintf(stderr, "Error: malloc failed\n");
+exit(98);
 }
+printf("%d\n", *p);
+free(p);
+return (0);
 }
