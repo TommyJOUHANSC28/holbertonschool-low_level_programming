3d80bb44329f6014302eacfbb23a4ae84a011453
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-17 15:33:20 +0000

    int index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
 * int_index - function that searches for an integer
 * @array: array
@@ -8,7 +10,7 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int  *p, i = 0;
+int  *p, i = 2;
 if (size <= 0)
 return (-1);
 for (p = &array[i]; p < &array[size - 1]; p++)
