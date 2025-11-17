ed73931a4bc4b663c6b9321e2e5c2f529b4e62d2
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-17 11:38:32 +0000

    Print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_name - a function that prints a name
+* @name: string for name
+* @f: pointer for all charactere
+* Return: value of f
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
