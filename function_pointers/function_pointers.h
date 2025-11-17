43506bcaad5c07729a7cd378b87ca2e8f849bbfa
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-17 14:03:42 +0000

    Array iterator.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -31,4 +32,7 @@ void print_diagsums(int *a, int size);
 void _puts_recursion(char *s);
 void set_string(char **s, char *to);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif
+
