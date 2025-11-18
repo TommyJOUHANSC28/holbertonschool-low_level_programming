ca80a5b84aafe70675ba1d326d130b22dcbf7d37
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-18 10:51:32 +0000

    Performs operaration

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * op_add - Additionne deux entiers
  * @a: premier entier
@@ -37,6 +39,11 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 /**
@@ -47,5 +54,10 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
