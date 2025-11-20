91a301b5ab1c98248e8feeb6165299e49298f3e0
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-20 16:13:27 +0000

    main opcodes

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+/**
+* main - A program that prints the opcodes of its own main function
+* @argc: the number of args
+* @argv: argument vector
+* Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+int i;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(2);
+}
+while (i < atoi(argv[1]))
+{
+printf("%02x", *((unsigned char *)main + i));
+i++;
+if (i < atoi(argv[1]))
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
