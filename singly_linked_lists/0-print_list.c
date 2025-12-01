b3dbb0c8f989874bf20d014efa47694caa4933e2
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-01 12:38:07 +0000

    singly_linked_lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* print_list - a function that prints all the elements of a list_t list
+* @h: pointeur of struture list_t
+* Return: the number of nodes printer
+*/
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+if (!h->str)
+printf("[8](nil)\n");
+else
+printf("[%d]%s\n", h->len, h->str);
+h = h->next;
+};
+return (i);
+}
