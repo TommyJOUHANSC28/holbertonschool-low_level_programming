48a46be4f25a8a17d5ce7ab89edfca59437b670a
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-02 12:16:37 +0000

    print dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,17 @@ size_t print_dlistint(const dlistint_t *h)
 {
 int data = 0;
 if (h == NULL)
+{
 return (0);
+}
 else 
+{
 while(h)
 {
 printf("%d\n", h->n);
 h = h->next;
 data++;
+}
 }
 return (data);
 }
