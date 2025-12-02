81ab4d7548bbc49ee433efefe7bf39a7eb5580bb
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-02 20:12:18 +0000

    number of elements

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,16 +2,15 @@
 /**
 * dlistint_len - function that returns the number
 * of elements in a linked dlistint_t list
-* @h: pointer constant the dlistint
+* @h: pointer head of constant the dlistint
 * Return: the number of node
 */
 size_t dlistint_len(const dlistint_t *h)
 {
-
 unsigned int nodeCount = 0;
 const dlistint_t *lister;
-lister = h;
 lister = malloc(sizeof(dlistint_t));
+lister = h;
 if (lister == NULL)
 {
 return (0);
