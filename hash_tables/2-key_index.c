30392951b185fa1829ada6998f0fcc19aba2c0fb
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-04 22:03:53 +0000

    key_index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,10 +1,9 @@
 #include "hash_tables.h"
 /**
-* key_index  - a function that gives you the index of a key.
+* key_index - a function that gives you the index of a key.
 * @key: the key
 * @size:the size of the array of the hash table
-*
-* Returns: the index at which the key or value pair
+* Return: value of the index at which the key or value pair
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
