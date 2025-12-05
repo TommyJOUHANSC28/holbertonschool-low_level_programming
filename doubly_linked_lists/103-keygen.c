f5d351ee6473fd75c7242b3a439bf4a248fc5d71
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-05 22:05:47 +0000

    keygen

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,93 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include <stdlib.h>
 /**
-* simple_mix - simple non-cryptographic mixing function
-* @data: input buffer
-* @len: length of buffer
+* main - generate a key depending on a username for crackme5
+* @argc: number of arguments passed
+* @argv: arguments passed to main
 *
-* Return: 64-bit accumulator
+* Return: 0 on success, 1 on error
 */
-static uint64_t simple_mix(const unsigned char *data, size_t len)
+int main(int argc, char *argv[])
 {
-uint64_t acc = 0x9E3779B97F4A7C15ULL; /* golden ratio */
-uint64_t prime = 0xC2B2AE3D27D4EB4FULL;
-size_t i;
-for (i = 0; i < len; i++)
-{
-acc ^= (uint64_t)data[i] + (acc << 7) + (acc >> 3);
-acc *= prime;
-acc ^= (acc >> 33);
-}
-/* final avalanche */
-acc ^= acc >> 29;
-acc *= 0x2545F4914F6CDD1DULL;
-acc ^= acc >> 32;
-return acc;
-}
-/**
-* format_key - render a key in a friendly segments (e.g., XXXXX-XXXXX)
-* @mix: 64-bit value to encode
-* @out: buffer for output (must be >= 32 bytes)
-*
-* Return: pointer to out
-*/
-static char *format_key(uint64_t mix, char *out)
-{
-const char alphabet[] = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"; /* no confusing chars */
-const size_t N = sizeof(alphabet) - 1;
-int i, pos = 0;
-/* 5x5 segments: XXXXX-XXXXX */
-for (i = 0; i < 10; i++)
-{
-int j;
-for (j = 0; j < 5; j++)
-{
-/* rotate and map to alphabet */
-mix ^= (mix << 13) ^ (mix >> 7);
-out[pos++] = alphabet[(mix + j + i) % N];
-}
-if (i != 9)
-out[pos++] = '-';
-}
-out[pos] = '\0';
-return out;
-}
-/**
-* main - entry point
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: 0 on success, non-zero on error
-*/
-int main(int argc, char **argv)
-{
-const char *user;
-size_t len;
-uint64_t mix;
-char key[64];
+unsigned int i, b;
+size_t len, add;
+char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+char p[7] = "      ";
 if (argc != 2)
 {
-fprintf(stderr, "Usage: %s username\n", argv[0]);
+printf("Correct usage: ./keygen5 username\n");
 return (1);
 }
-user = argv[1];
-len = strlen(user);
-if (len == 0)
-{
-fprintf(stderr, "Error: username must be non-empty.\n");
-return (2);
-}
-/* optional normalization: trim spaces, enforce ASCII, etc. */
-mix = simple_mix((const unsigned char *)user, len);
-/* add sanity guards derived from username properties */
-mix ^= (uint64_t)len * 0x1000003ULL;
-if (user[0] != '\0')
-mix ^= (uint64_t)(unsigned char)user[0] << 32;
-if (user[len - 1] != '\0')
-mix ^= (uint64_t)(unsigned char)user[len - 1] << 16;
-format_key(mix, key);
-printf("%s\n", key);
+len = strlen(argv[1]);
+p[0] = l[(len ^ 59) & 63];
+for (i = 0, add = 0; i < len; i++)
+add += argv[1][i];
+p[1] = l[(add ^ 79) & 63];
+for (i = 0, b = 1; i < len; i++)
+b *= argv[1][i];
+p[2] = l[(b ^ 85) & 63];
+for (b = argv[1][0], i = 0; i < len; i++)
+if ((char)b <= argv[1][i])
+b = argv[1][i];
+srand(b ^ 14);
+p[3] = l[rand() & 63];
+for (b = 0, i = 0; i < len; i++)
+b += argv[1][i] * argv[1][i];
+p[4] = l[(b ^ 239) & 63];
+for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+b = rand();
+p[5] = l[(b ^ 229) & 63];
+printf("%s\n", p);
 return (0);
 }
