08925dee6c207d7c7a0545d68ca67a0f9d3a51da
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-05 13:50:10 +0000

    creat_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,8 +2,8 @@
 /**
 * create_file - Function that creates a file
 * @filename: The name of the file to create
-* @text_content:  string to write to the file
-* return: 1 on success or -1 on failure
+* @text_content: string to write to the file
+* Return: 1 on success or -1 on failure
 */
 int create_file(const char *filename, char *text_content)
 {
